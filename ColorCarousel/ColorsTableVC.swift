5d5b40dd36e12abcfdfefa66a79e706980eb61cd
cesarlopezt <cesarlopez2808@gmail.com>
2022-10-18 08:30:35 -0400

    Add colors table and pass color to detail

--- ColorCarousel/ColorsTableVC.swift
+++ ColorCarousel/ColorsTableVC.swift
@@ -9,24 +9,49 @@ import UIKit
 
 class ColorsTableVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var colors: [UIColor] = []
+    
+    enum Cells {
+        static let colorCell = "ColorCell"
     }
     
-
-    @IBAction func tempButtonTapped(_ sender: UIButton) {
-        performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
+    enum Segues {
+        static let toDetail = "ToColorsDetailVC"
     }
-    /*
-    // MARK: - Navigation
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadColors()
+    }
+    
+    func loadColors() {
+        for _ in 0..<50{
+            colors.append(UIColor.random())
+        }
+    }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let destinationVC = segue.destination as! ColorsDetailVC
+        destinationVC.color = sender as? UIColor
     }
-    */
+}
 
+extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Cells.colorCell) else {
+            return UITableViewCell()
+        }
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let color: UIColor = colors[indexPath.row]
+        performSegue(withIdentifier: Segues.toDetail, sender: color)
+    }
+    
 }
---
+++ ColorCarousel/UIColor+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Ext.swift
+//  ColorCarousel
+//
+//  Created by Cesar Lopez on 10/18/22.
+//
+
+import UIKit
+
+extension UIColor {
+    static func random () -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0...1),
+                       green: CGFloat.random(in: 0...1),
+                       blue: CGFloat.random(in: 0...1),
+                       alpha: 1)
+    }
+    
+}
